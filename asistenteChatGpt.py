fa7fe99ddd79c5a9c50d3b81ebd9848ae52bca35
Antonio Gallardo <agg860@inlumine.ual.es>
2025-08-30 16:17:53 +0200

    Puesto puertos correctos

--- asistenteChatGpt.py
+++ asistenteChatGpt.py
@@ -60,4 +60,5 @@ def preguntar_web():
     return f"<p><b>Pregunta:</b> {mensaje}</p><p><b>Respuesta:</b> {respuesta}</p><a href='/'>Volver</a>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
